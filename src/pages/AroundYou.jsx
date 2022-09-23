f32944356f30ff02afeab201c5ffa98f287b8be2
Hussein Omar <husseinomar6190@gmail.com>
2022-09-24 01:42:25 +0300

    creating AroundYou page to display songs around you

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,5 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
-const CountryTracks = () => <div>CountryTracks</div>;
+import { Error, Loader, SongCard } from '../components';
+import { useGetSongsByCountryQuery } from '../redux/services/shazamCore';
+
+const CountryTracks = () => {
+  const [country, setCountry] = useState('');
+  const [loading, setLoading] = useState(true);
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  let available = [
+    'DE,PT,DK,HR,UA,HU,MA,ID,IE,US,EG,IL,UY,IN,ZA,IT,MX…G,BY,GB,CA,CH,KR,CL,GR,CN,CO,KZ,CR,TH,PE,CZ,PL,TR',
+  ];
+
+  console.log(country);
+  useEffect(() => {
+    axios
+      .get(
+        `https://geo.ipify.org/api/v2/country?apiKey=at_QUaF3el0dW60uQp0RRhX2MOhwWJiW`
+      )
+      .then((res) => setCountry(res?.data?.location.country))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  }, [country]);
+  console.log(country);
+  const getCorrectCountry = (country) => {
+    if (available.includes(country)) return country;
+    return 'US';
+  };
+
+  const { data, isFetching, error } = useGetSongsByCountryQuery(
+    getCorrectCountry(country)
+  );
+  console.log(data);
+
+  if (isFetching && loading)
+    return <Loader title='Loading Songs around you...' />;
+
+  if (error && country !== '') return <Error />;
+
+  return (
+    <div className='flex flex-col'>
+      <h2 className='font-bold text-3xl text-white text-left mt-4 mb-10'>
+        Around you <span className='font-black'>{country}</span>
+      </h2>
+
+      <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
+        {data?.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+            i={i}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default CountryTracks;
